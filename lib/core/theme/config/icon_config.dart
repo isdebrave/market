eb30d2de8c6800ebbae80f7c34dc15f8cbb4087a
Kim Seok Yoon <isdebrave@gmail.com>
2025-02-13 00:44:58 +0900

    Update: icon_config 설정

---
+++ lib/core/theme/config/icon_config.dart
@@ -0,0 +1,6 @@
+class IconConfig {
+  static const String mainLogo = 'assets/svg/main_logo.svg';
+
+  static const String location = 'assets/svg/icon-location.svg';
+  static const String cart = 'assets/svg/icon-cart.svg';
+}
