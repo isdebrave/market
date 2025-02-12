0bbdc1ec82e942ded2b8d62a3bd06ecf0478f92d
Kim Seok Yoon <isdebrave@gmail.com>
2025-02-13 00:44:07 +0900

    Update: splash screen 생성

---
+++ lib/splash_screen.dart
@@ -0,0 +1,40 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:go_router/go_router.dart';
+import 'package:market/core/theme/config/icon_config.dart';
+import 'package:market/home_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  static const String routeName = 'SplashScreen';
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Timer(
+      const Duration(seconds: 2),
+      () => context.goNamed(HomeScreen.routeName),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        body: AnnotatedRegion<SystemUiOverlayStyle>(
+          value: SystemUiOverlayStyle.light,
+          child: Center(
+            child: SvgPicture.asset(IconConfig.mainLogo),
+          ),
+        ),
+      );
+}
